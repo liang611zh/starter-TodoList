b544cd416e2ddbd3f5d05981df192b5ef9c216d0
Liang Zhao <zhaoliangbz@gmail.com>
2017-10-12 12:21:44 -0700

    add home page features closes#2

---
+++ application/views/homepage.php
@@ -0,0 +1,12 @@
+<div class="alert alert-info">{remaining_tasks} tasks are left to do!</div>
+<table class="table">
+    <tr>
+		{display_tasks}
+		<tr>
+		    <td>{id}</td>
+		    <td>{task}</td>
+		    <td>{priority}</td>
+		</tr>
+		{/display_tasks}
+    </tr>
+</table>
